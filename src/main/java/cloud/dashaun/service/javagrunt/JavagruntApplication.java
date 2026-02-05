b16ada75fbc49bbe7d19d72eb8634758eda2ac5b
DaShaun <dashaun@dashaun.com>
2026-02-05 17:19:08 -0600

    initializr

---
+++ src/main/java/cloud/dashaun/service/javagrunt/JavagruntApplication.java
@@ -0,0 +1,13 @@
+package cloud.dashaun.service.javagrunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagruntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavagruntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/dashaun/service/javagrunt/JavagruntApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.dashaun.service.javagrunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagruntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
